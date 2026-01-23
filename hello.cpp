d6c24b856130524c4c582f7929be8fb920c1888c
anonymouscode7870 <anonymouscoder8910@gmail.com>
2026-01-22 17:05:23 -0800

    pandas

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,25 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-    cout<<"hello";
-    
-}   
+
+void f(vector<int>& dp, int n) {
+    if (n == 0) return;
+
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> dp(n + 1, -1);
+    f(dp, n);
+
+    cout << dp[n];
+    return 0;
+}
