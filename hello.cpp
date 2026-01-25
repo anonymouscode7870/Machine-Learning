c99f69de969c7d81f2430e24e03e36c734f48037
anonymouscode7870 <anonymouscoder8910@gmail.com>
2026-01-24 16:02:45 -0800

    learn

--- hello.cpp
+++ hello.cpp
@@ -1,25 +1,16 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-void f(vector<int>& dp, int n) {
-    if (n == 0) return;
-
-    dp[0] = 0;
-    dp[1] = 1;
-
-    for (int i = 2; i <= n; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2];
+int gcd(int a, int b) {
+    while (b != 0) {
+        int rem = a % b;
+        a = b;
+        b = rem;
     }
+    return a;
 }
 
 int main() {
-    int n;
-    cin >> n;
-
-    vector<int> dp(n + 1, -1);
-    f(dp, n);
-
-    cout << dp[n];
-    return 0;
+    int a = 12, b = 18;
+    cout << "GCD = " << gcd(a, b) << endl;
 }
