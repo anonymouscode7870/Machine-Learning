2b9444d5c28506c7885906e9dcb7d20693f41ec2
anonymouscode7870 <anonymouscoder8910@gmail.com>
2026-01-24 17:27:29 -0800

    learning

---
+++ TypeScript/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var age = 25;
+if (age < 18) {
+    console.log("Minor");
+}
+else {
+    console.log("Adult");
+}
+//# sourceMappingURL=index.js.map
---
+++ TypeScript/src/index.ts
@@ -0,0 +1,7 @@
+var age: number = 25;
+if(age < 18) {
+    console.log("Minor");
+} else {
+    console.log("Adult");
+}
+
